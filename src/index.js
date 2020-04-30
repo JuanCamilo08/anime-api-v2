4e85509693aac16a539ef91fbad9720631262acd
JuanCamilo08 <richin12345@gmail.com>
2020-04-30 15:23:17 -0400

    add eslint, prettier and nvm

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import '@babel/polyfill';
 import Hapi from '@hapi/hapi';
 
 const init = async () => {
-  const server = Hapi.server({ host: 'localhost', port: '3000'});
+  const server = Hapi.server({ host: 'localhost', port: '3000' });
 
   await server.start();
   console.log(`server running on ${server.info.uri}`);
-}
+};
 
 init();
-
