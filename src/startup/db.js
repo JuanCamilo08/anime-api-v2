0bd64ca78503e5ab34de527893a5da88fadfa096
JuanCamilo08 <richin12345@gmail.com>
2020-05-28 11:31:11 -0400

    improving to make the environment better

--- src/startup/db.js
+++ src/startup/db.js
@@ -1,25 +1,22 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(
-  'postgres',
-  process.env.POSTGRES_DB,
-  process.env.POSTGRES_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'postgres',
-    pool: {
-      max: 5,
-      min: 0,
-      require: 30000,
-      iddle: 10000,
-    },
-  }
-);
+const { POSTGRES_DB, POSTGRES_PASSWORD, POSTGRES_HOST } = process.env;
+
+const sequelize = new Sequelize('postgres', POSTGRES_DB, POSTGRES_PASSWORD, {
+  host: POSTGRES_HOST || 'localhost',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    require: 30000,
+    iddle: 10000,
+  },
+});
 
 // console.log(models)
 export default sequelize;
 
-exports.dbInit = async function () {
+exports.dbInit = async () => {
   try {
     await sequelize.authenticate();
     console.log(`db on...`);
